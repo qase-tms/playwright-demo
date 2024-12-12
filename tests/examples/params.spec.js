1f97424f9d9829a0d67dbe48e67e2dbcbec6cf51
Manju R <cskmnrpt@gmail.com>
2024-12-12 17:29:18 +0530

    add param.spec.js

--- tests/examples/params.spec.js
+++ tests/examples/params.spec.js
@@ -1,17 +1,35 @@
 import { test, expect } from '@playwright/test';
 import { qase } from 'playwright-qase-reporter'; 
 
-[
-  { username: '@alice', expected: 'Hello, Alice!' },
-  { username: '@bob', expected: 'Hello, Bob!' },
-  { username: '@charlie', expected: 'Hello, Charlie!' },
-].forEach(({ username, expected }) => {
+const testCases = [
+  { username: '@alice', browser: 'Chromium' },
+  { username: '@bob', browser: 'Firefox' },
+  { username: '@charlie', browser: 'Firefox' },
+];
+//].forEach(({ username, browser }) => {
 
+testCases.forEach(({ username }) => {
   test(`Test login with ${username}`, async () => {
     qase.title("Verify if user is able to login.");
 
     qase.parameters({ 'Username': username }); 
 
+    expect(true).toBe(true);
+  });
+});
+
+testCases.forEach(({ username, browser }) => {
+  test(`Test login with ${username} using qase.groupParameters`, async () => {
+    // Set the test title
+    qase.title("Verify if user is able to login with grouped parameters.");
+
+    // Group parameters for better organization
+    qase.groupParameters({
+      'Browser': browser,
+      'Username': username,
+    });
+
+    // Assertion for demonstration purposes
     expect(true).toBe(true);
   });
 });
