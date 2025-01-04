ec29c184c26700b57e59a000dcca36e4c59f2e30
Manju R <cskmnrpt@gmail.com>
2025-01-04 18:57:34 +0530

    mod: Playwright config
    Added projects to the playwright config file.

--- playwright.config.js
+++ playwright.config.js
@@ -1,6 +1,45 @@
 import { defineConfig } from "@playwright/test";
 
 export default defineConfig({
+  projects: [
+    {
+      name: "Chromium",
+      use: {
+        browserName: "chromium",
+        headless: false,
+        viewport: { width: 1280, height: 720 },
+        userAgent:
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
+        video: "on-first-retry",
+        screenshot: "only-on-failure",
+        geolocation: { longitude: 12.4924, latitude: 41.8902 },
+        locale: "en-US",
+      },
+    },
+    {
+      name: "Firefox",
+      use: {
+        browserName: "firefox",
+        headless: true,
+        viewport: { width: 1024, height: 768 },
+        video: "on",
+        screenshot: "on",
+        storageState: "state.json",
+      },
+    },
+    {
+      name: "Webkit",
+      use: {
+        browserName: "webkit",
+        headless: false,
+        viewport: { width: 1280, height: 720 },
+        video: "on-first-retry",
+        geolocation: { longitude: -73.935242, latitude: 40.73061 },
+        locale: "fr-FR",
+        bypassCSP: true,
+      },
+    },
+  ],
   reporter: [
     [
       "playwright-qase-reporter",
